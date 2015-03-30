77c555c389323eb7283cf161fcb9794bca2da154
shk33 <m.coronel.seg@gmail.com>
2015-03-30 16:59:23 -0600

    Corerct typo

--- app/views/layouts/sidebar.blade.php
+++ app/views/layouts/sidebar.blade.php
@@ -20,7 +20,7 @@
             <li>
               <a href="index.html">
                 <i class="fa fa-dashboard"></i>
-                <span class="menu-title"><strong>Estadśiticas</strong></span>
+                <span class="menu-title"><strong>Estadísticas</strong></span>
               </a>
             </li>
 
