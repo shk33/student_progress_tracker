364f897d4d3c24f5ef640ac30149feef80ad9142
Miguel Coronel <m.coronel.seg@gmail.com>
2015-04-14 09:41:42 -0500

    Remove nullable to scholar group description

--- app/database/migrations/2015_04_14_142509_create_scholar_groups_table.php
+++ app/database/migrations/2015_04_14_142509_create_scholar_groups_table.php
@@ -15,7 +15,7 @@ class CreateScholarGroupsTable extends Migration {
 		Schema::create('scholar_groups', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name')->nullable();
+			$table->string('name');
 			$table->text('description')->nullable();
 			$table->integer('user_id')->unsigned()->nullable();
 			$table->foreign('user_id')->references('id')->on('users')
