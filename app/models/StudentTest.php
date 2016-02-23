ed8dd8a9457757ef6163dab7e9ff008c13dc0b2d
Miguel Coronel <m.coronel.seg@gmail.com>
2016-02-23 15:25:02 -0600

    Add student_test model and migrations

---
+++ app/models/StudentTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class StudentTest extends \Eloquent {
+  /**
+   * The database table used by the model.
+   *
+   * @var string
+   */
+  protected $table = 'student_tests';
+  
+	// Add your validation rules here
+  public static $rules = [
+    'name'    => 'required',
+  ];
+
+  // Don't forget to fill this array
+  protected $fillable = ["name","type"];
+}
