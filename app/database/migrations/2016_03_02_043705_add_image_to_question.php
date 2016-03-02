97cde0129d446b65898bde2164f05548c2fd003f
Miguel Coronel <m.coronel.seg@gmail.com>
2016-03-01 23:03:48 -0600

    Add image to questions table

---
+++ app/database/migrations/2016_03_02_043705_add_image_to_question.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddImageToQuestion extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('questions', function(Blueprint $table)
+		{
+			$table->string('image');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('questions', function(Blueprint $table)
+		{
+			$table->dropColumn('image');
+		});
+	}
+
+}
