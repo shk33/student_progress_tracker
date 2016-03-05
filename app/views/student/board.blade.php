d1f2528443c4cf689ecefa8391a5e1538a617076
buirai <buirai_naetle@hotmail.com>
2016-03-05 11:51:50 -0600

    panel fill

--- app/views/student/board.blade.php
+++ app/views/student/board.blade.php
@@ -2,22 +2,27 @@
 
 @section('content')
 
-  <!-- Maint Content -->
-  <div id="page-content">
-    
-    <div class="row">
-      <div class="col-md-12 col-lg-12">
-        <div class="panel">
+	<!-- Maint Content -->
+	<div id="page-content">
+		
+		<div class="row">
+			<div class="col-md-12 col-lg-12">
+				<div class="panel">
 
-          <div class="panel-heading">
-            
-          </div>
-          
-        </div>
-      </div>
-    </div>
+					<div class="panel-heading">
+						<h3 class="panel-title">
+							Panel de estudiante  
+						</h3>
+					</div>
 
-  </div>
+					<div class="panel-body">
+						@include('student.partials.index_games')
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
 
 
 @stop
---
+++ app/views/student/partials/index_games.blade.php
@@ -0,0 +1,39 @@
+<div class="row">
+	<div class="col-md-6 text-center">
+		<h4 class="text-center">Ingresa al juego 1</h4>
+
+		<img src="{{ asset('images/landing/ser02.png') }}", alt="Juego 1">
+		<a href="#">
+			<div class="panel media pad-all col-md-6 col-md-offset-3">
+				<div class="media-left">
+					<span class="icon-wrap icon-wrap-sm icon-circle bg-info">
+						<i class="fa fa-play fa-2x"></i>
+					</span>
+				</div>
+				<div class="media-body">
+					<p class="text-2x mar-no text-thin">Jugar</p>
+					<p class="text-muted mar-no">< nombre del juego 1 ></p>
+				</div>
+			</div>
+		</a>
+	</div>
+
+	<div class="col-md-6 text-center">
+		<h4 class="text-center">Ingresa al juego 1</h4>
+
+		<img src="{{ asset('images/landing/ser02.png') }}", alt="Juego 2">
+		<a href="#">
+			<div class="panel media pad-all col-md-6 col-md-offset-3">
+				<div class="media-left">
+					<span class="icon-wrap icon-wrap-sm icon-circle bg-info">
+						<i class="fa fa-play fa-2x"></i>
+					</span>
+				</div>
+				<div class="media-body">
+					<p class="text-2x mar-no text-thin">Jugar</p>
+					<p class="text-muted mar-no">< nombre del juego 1 ></p>
+				</div>
+			</div>
+		</a>
+	</div>
+	</div>
